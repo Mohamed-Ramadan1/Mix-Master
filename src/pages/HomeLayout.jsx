7034caa1ead172853df0ddc9798d68935fd1ddf3
Mohamed Ramadan <123234014+Mohamed-Ramadan1@users.noreply.github.com>
2023-09-15 20:36:24 +0300

    Adding styled component and wrappers.

--- src/pages/HomeLayout.jsx
+++ src/pages/HomeLayout.jsx
@@ -1,10 +1,13 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
+import About from "./About";
 const HomeLayout = () => {
   return (
     <>
       <Navbar />
-      <Outlet />
+      <section className="page">
+        <Outlet />
+      </section>
     </>
   );
 };
